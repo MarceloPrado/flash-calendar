49a31994745fed6ce9f79ada148a0fce970703cd
Marcelo T Prado <mprado@brex.com>
2024-02-19 07:04:38 -0300

    fix eslint error

--- packages/flash-calendar/.eslintrc.js
+++ packages/flash-calendar/.eslintrc.js
@@ -7,11 +7,6 @@ module.exports = {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-      },
-    },
   },
   rules: {
     "no-restricted-imports": [
