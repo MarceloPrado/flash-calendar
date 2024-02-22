b9a9a3bb349103bdd2fccabfe6e030e9a6d93bf2
Marcelo T Prado <mprado@brex.com>
2024-02-22 12:16:02 -0300

    add gh action for docs deployment

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -26,8 +26,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "marceloprado", // Usually your GitHub org/user name.
+  projectName: "flash-calendar", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
