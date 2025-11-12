06390fe338aa1805d54997dfb29cacbee4ce4d9c
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-11-12 12:26:05 +0000

    fix: update docusaurus

--- apps/example/.rnstorybook/preview.tsx
+++ apps/example/.rnstorybook/preview.tsx
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react-native";
 import { backgroundDecorator } from "@/developer/decorators";
 
 const preview: Preview = {
